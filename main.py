71909202ac5e814038c62da7840105d55493162e
Anton <ant.trifonenkov@yandex.ru>
2021-12-18 16:15:46 +0300

    add function main() use PEP8 for another functions

--- main.py
+++ main.py
@@ -1,52 +1,77 @@
 import random
+import json
+
 BOOKS = "books.txt"
+INPUT_FILE = "input.json"
 
 
-def var_from_conf():
+def main():
     from conf import model
-    print(model)
 
+    to_json = {"model": model,
+               "fields": {
+                    "title": title(),
+                    "year": year(),
+                    "pages": pages(),
+                    "isbn13": isbn13(),
+                    "rating": rating(),
+                    "price": price(),
+                    "author": author()
+                        }
+               }
+
+    with open(INPUT_FILE, "w", encoding="utf-8") as f:
+        json.dump(to_json, f, indent=4, ensure_ascii=False)
+
+
+# Вытаскиваем название книги из books.txt
 def title():
     with open(BOOKS, "r", encoding="utf-8") as f:
-        a = []
-        for list in f:
-            a.append(list)
-        z = random.choice(a)
-        n = z.replace("\n", "")
-        print(n)
+        book_list = []
+        for i in f:
+            book_list.append(i)
+        random_book = random.choice(book_list)
+        book = random_book.replace("\n", "")
+        return book
 
+
+# Делаем ранодомный год
 def year():
-    y = random.randrange(1990, 2021, 1)
-    print(y)
+    some_year = random.randrange(1990, 2021, 1)
+    return some_year
 
+
+# Делаем ранодомную страницу
 def pages():
-    y = random.randrange(1, 500, 1)
-    print(y)
+    some_page = random.randrange(1, 500, 1)
+    return some_page
 
+
+# Делаем фейковый книжный номер, по стандарту isbn13
 def isbn13():
     from faker import Faker
     fake = Faker()
-    print(fake.isbn13())
+    return fake.isbn13()
 
+
+# Делаем ранодомный рейтинг
 def rating():
-    y = random.uniform(0.0, 5.0)
-    print('%.1f' % y)
+    some_rat = random.uniform(0.0, 5.0)
+    return float('%.1f' % some_rat)
 
+
+# Делаем ранодомную цену
 def price():
-    y = random.uniform(1.0, 1500.0)
-    print('%.1f' % y)
+    some_price = random.uniform(1.0, 1500.0)
+    return float('%.1f' % some_price)
 
+
+# Делаем фейкового автора книги
 def author():
     from faker import Faker
     fake = Faker(locale="ru_RU")
-    print(fake.name())
+    return fake.name()
+
 
 if __name__ == "__main__":
-    var_from_conf()
-    title()
-    year()
-    pages()
-    isbn13()
-    rating()
-    price()
-    author()
+    main()
